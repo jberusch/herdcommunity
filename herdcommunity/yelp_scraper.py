220b282eba8c9704ccf97465a56e6e0f60852126
Joe Berusch <joeberusch@gmail.com>
2019-12-26 10:07:15 -0600

    added domain

--- herdcommunity/yelp_scraper.py
+++ herdcommunity/yelp_scraper.py
@@ -9,9 +9,10 @@ from app.models import Destination
 
 BASE_URL = 'https://www.yelp.com/'
 NASHVILLE_SEARCH_TERM = 'search?find_desc=&find_near=vanderbilt-university-nashville-4&start=' # needs multiple of 10 (starting at 0) added to end
+CLEVELAND_SEARCH_TERM = 'search?find_desc=restaurants&find_loc=Shaker%20Heights%2C%20OH&ns=1&start='
 
 # scrape one page of results (they're paginated)
-def scrape_one_page(page_content):
+def scrape_one_page(page_content, region):
     # get all list items, where restaurants are stored
     list_items = page_content.find_all('li')
     for li in list_items:
@@ -44,19 +45,18 @@ def scrape_one_page(page_content):
             name=name,
             img_src=img_src,
             yelp_link=yelp_link if len(yelp_link) < 150 else BASE_URL,
-            region='Nashville',
+            region=region,
             num_visits=0,
             users=[]
         )
         print(new_dest)
         db.session.add(new_dest)
 
-
 def scrape_n_pages(n):
     for i in range(n):
         resp = requests.get(BASE_URL + NASHVILLE_SEARCH_TERM + str(i * 10), timeout=5)
         content = BeautifulSoup(resp.content, "html.parser")
-        scrape_one_page(content)
+        scrape_one_page(content, 'Nashville')
     db.session.commit()
 
 scrape_n_pages(138)
