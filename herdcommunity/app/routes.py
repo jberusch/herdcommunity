92555539ebd35f6445bf59226c5355d941d875e8
Joe Berusch <joeberusch@gmail.com>
2020-01-20 08:57:34 -0600

    changed user page title

--- herdcommunity/app/routes.py
+++ herdcommunity/app/routes.py
@@ -222,7 +222,7 @@ def signup():
         db.session.commit()
         # log in new user
         login_user(new_user, remember=True)
-        # ulog('signup -> new signup by user {}'.format(new_user))
+
         # notify admins that new user signed up
             # NOTE: NOT WORKING
             # send_email('New User Signup', app.config['ADMINS'][0], ['asroth43@gmail.com'], '', '<h2>New User Signup</h2>')
